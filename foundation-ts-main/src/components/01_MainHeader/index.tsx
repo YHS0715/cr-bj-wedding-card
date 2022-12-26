09f92a6895c85798c5a1a6294b4ac08910685672
jenny-han <dev.jennyhan@gmail.com>
2022-12-26 13:54:20 +0900

    feat : font 추가 및 스타일 조정 MainHeader 컴포넌트 구성

---
+++ foundation-ts-main/src/components/01_MainHeader/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './mainHeader.module.scss'
+
+import frameimages  from 'static/img/main_img.png'
+import mainimg from 'static/img/main1.jpeg'
+
+const MainHeader = () => {
+    return (
+        <div className={styles.title_box_wrap}>
+            { /* Header Main 페이지 입니다. 
+                들어가는 내용 : 
+                오프닝 메세지
+                메인 사진
+                예식 일시
+                */}
+            <img className={styles.frame} src={frameimages} />
+            <img className={styles.main_img} src={mainimg} />
+            <div className={styles.names}>
+                범준 ♥ 채림&nbsp;결혼합니다
+            </div>
+            <div className={styles.wd_date}>2023년 02월 18일(토) 낮 12시 30분</div>
+            <div className={styles.wd_info}>더리버사이드호텔 7층 콘서트홀</div>
+        </div>
+    )
+}
+
+export default MainHeader
--- foundation-ts-main/src/routes/Main/index.tsx
+++ foundation-ts-main/src/routes/Main/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import MainHeader from 'components/MainHeader'
-import MainMessage from 'components/MainMessage'
+import MainHeader from 'components/01_MainHeader'
+import MainMessage from 'components/02_MainMessage/MainMessage'
 import Greeting from 'components/Greeting'
 import CalendarDday from 'components/CalendarDday'
 import AccountList from 'components/AccountList'
@@ -17,13 +17,13 @@ const Main = () => {
           <MainHeader />
           {/* 일정 안내 및 안내글. */}
           <MainMessage />
-          {/*신랑 신부 감사인사. */}        
+          {/* 신랑 신부 감사인사.  */}        
           <Greeting />  
           {/* 디데이 캘린더. */}
           <CalendarDday />
           {/* 계좌 안내. */}
           <AccountList />
-          {/* 갤러리형 사진*/}
+          {/* 갤러리형 사진 */}
           <PhotoGallary />
           {/* 식순, 오시는길, 안내사항  */}
           <Notice />
