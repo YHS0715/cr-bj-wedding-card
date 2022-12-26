bb1459f4435e68ada861f87538133371ef8778c4
jenny-han <dev.jennyhan@gmail.com>
2022-12-26 18:12:40 +0900

    fix route 변경

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import Routes from './routes'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes />
     </BrowserRouter>
   </React.StrictMode>
